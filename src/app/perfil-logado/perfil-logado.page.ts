174a29540c789129bbd94e5bfcee05ca3baea652
Marcos <marcos.silva3@estudante.ifb.edu.br>
2019-04-10 11:13:53 -0300

    Aprimorado botões; Padronizado botões; Padronizado texto login das Telas Eventos e Perfil; Cadastrar e esqueci senhas em botões diferentes; Adicionado botão de sair, invés de voltar após fazer login tanto no perfil quanto eventos.

--- src/app/perfil-logado/perfil-logado.page.ts
+++ src/app/perfil-logado/perfil-logado.page.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-info-conecta',
-  templateUrl: './info-conecta.page.html',
-  styleUrls: ['./info-conecta.page.scss'],
+  selector: 'app-perfil-logado',
+  templateUrl: './perfil-logado.page.html',
+  styleUrls: ['./perfil-logado.page.scss'],
 })
-export class InfoConectaPage implements OnInit {
+export class PerfilLogadoPage implements OnInit {
 
   constructor(
     private navCtrl: NavController
