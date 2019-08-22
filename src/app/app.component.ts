31555fc8cd9b5c217ee12d176f36616679c6a035
Marcos Allysson <marcos.silva3@estudante.ifb.edu.br>
2019-08-22 08:57:40 -0300

    sidemenu fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,21 @@ import { Platform, AlertController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+  public appPages = [
+    //{title: 'Feed',url: '/home',icon: 'home'}, // references home
+    {title: 'Horário de Funcionamento do IFB',url: '/horario',icon: 'time'}, // references home
+    {title: 'Redes Sociais',url: '/rede-social',icon: 'md-at'},
+    {title: 'Sobre',url: '/sobre',icon: 'information'},
+    {title: 'Desenvolvedores',url: '/desenvolvedores',icon: 'person'},
+    {title: 'Administrativo',url: '/perfil',icon: 'cog'}
+  ];
+        
   constructor(
     public platform: Platform,
     public splashScreen: SplashScreen,
